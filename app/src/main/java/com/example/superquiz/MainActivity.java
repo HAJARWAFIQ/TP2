3833a4b7f5aab0a4ac7c014ba673e8a68a6fc564
Wafiq Hajar <wafiqhajaar@gmail.com>
2024-03-05 10:24:21 +0100

    Ajout du code initial

---
+++ app/src/main/java/com/example/superquiz/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.superquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.first_project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
